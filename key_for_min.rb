75419f2ba55253a8f76c76f96363b35bcce471bb
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-21 14:15:38 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,8 +8,6 @@ def key_for_min_value(name_hash)
   name_hash.each do |key,value|
     if !min_value || min_value<value
       min_value=value
-    else
-      return nil
     end
     # if name_hash[key]==min_value
     #   return key
