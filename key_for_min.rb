376ba82a90dd2ed592e62a4caf9868a98030e48d
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-21 14:12:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
-binding.pry
-  min_value=nil
+# binding.pry
+  # min_value=nil
   name_hash.each do |key,value|
     if !min_value || min_value<value
       min_value=value
